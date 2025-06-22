4bc92386f1260153b83ddf3ea53dbc20bbdd5646
Daniela-Palyan <palyandaniela@gmail.com>
2025-06-22 14:59:54 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
     user = manager.login("alice", "123");
     if (user) {
         manager.add_task(user, "Do Homework", "Finish math and CS", "2025-07-01", "Study", MID, NOT_STARTED);
-        user->listTasks();
+        user->list_tasks();
     } else {
         std::cout << "Login failed!" << std::endl;
     }
